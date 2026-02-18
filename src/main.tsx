c3dcc90906f3f0e6197d80e697fc654f9ad22270
bouix <guillermo.bouix@gmail.com>
2026-02-18 20:01:12 -0300

    Agregar vitest como dependencia y eliminar comentarios innecesarios en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-// Punto de entrada de la aplicación React
-// Se monta el componente principal App en el elemento del DOM con id "root"
-
 import { createRoot } from "react-dom/client";
-
 import "./globals.css";
-
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(<App />);
